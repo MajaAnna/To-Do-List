c1d3dc33e127effd0cae6b3aa1ec7f47a6834ab7
OleksandrMitrakov <mitrakovoleksandr@gmail.com>
2018-03-22 16:48:28 +0100

    btn

--- js/js.js
+++ js/js.js
@@ -7,21 +7,10 @@ document.addEventListener('DOMContentLoaded', function () {
         authorName = document.querySelector('#name'),
         priority = document.querySelector('#priority'),
         comment = document.querySelector('#comment'),
-        table = document.getElementById('table'),
-        tableBody = document.getElementById('tbody'),
+        removeButton = document.querySelector('#removeFinishedTasksButton'),
+
 
         inputControl = document.getElementsByClassName('validation_required');
-
-    /* Nie wiem gdzie wszdzić */
-
-    function showHideTable() {
-
-        if (tableBody.children.length === 0) {
-            table.hidden = true;
-        } else {
-            table.hidden = false;
-        }
-    }
 
     /*Przycisk pokazuje formularz do dodawania zadania*/
 
@@ -36,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /* walidacja danych */
 
-        for (var i=0; i<=inputControl.length; i++) {
+        for (var i=0; i<inputControl.length; i++) {
             if(inputControl[i] === undefined)
             {
                 continue;
@@ -51,21 +40,59 @@ document.addEventListener('DOMContentLoaded', function () {
             tdAdditionDate = document.createElement('td'),
             tdAuthor = document.createElement('td'),
             tdPriority = document.createElement('td'),
-            tdTask = document.createElement('td');
+            tdTask = document.createElement('td'),
+            btnDelete = document.createElement('button'),
+            btnComplete = document.createElement('button');
+
+            btnDelete.innerText = 'Usunąć';
+            btnComplete.innerText = 'Wykonane';
+
+        btnDelete.setAttribute("style", "background-color: rgba(255, 3, 21, 0.65);");
+        btnComplete.setAttribute("style", "background-color: rgba(0, 128, 0, 0.65);");
+        removeButton.setAttribute("style", "background-color: rgba(217, 213, 0, 0.65)");
 
             tdAdditionDate.innerText = additionDate.value;
             tdAuthor.innerText = authorName.value;
             tdPriority.innerText = priority.value;
             tdTask.innerText = comment.value;
 
+            //czyszczenie inputa po zapisaniu zadania
+
+            additionDate.value = '';
+            authorName.value = '';
+            priority.value = '';
+            comment.value = '';
+
+            //zadanie wykonane
+            btnComplete.addEventListener('click', function () {
+                tr.classList.toggle('complete');
+            });
+
+            //usuwanie zadania z listy
+            btnDelete.addEventListener('click', function () {
+                this.parentElement.parentElement.removeChild(this.parentElement);
+            });
+
             tr.appendChild(tdAdditionDate);
             tr.appendChild(tdAuthor);
             tr.appendChild(tdPriority);
             tr.appendChild(tdTask);
+            tr.appendChild(btnComplete);
+            tr.appendChild(btnDelete);
 
             table.appendChild(tr);
-/*Po dodaniu zadania, formularz się chowa.*/
+
+            /*Po dodaniu zadania, formularz się chowa.*/
             form.hidden = !form.hidden;
+
+            //usuwanie ukończonych zadań
+            removeButton.addEventListener('click', function () {
+                if (tr.className.indexOf('complete') > -1) {
+                    tr.parentElement.removeChild(tr);
+                    console.log('Ok');
+                }
+
+            });
 
     });
 
